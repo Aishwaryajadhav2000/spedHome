4cee089932cbbe503bdc60c159cd7c0288ae751d
aishwarya <aishwaryaj1608@gmail.com>
2025-06-28 12:40:07 +0530

    njfv

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './components/dashboard/dashboard';
 import { Page1 } from './components/page1/page1';
-
+import { Page2 } from './components/page2/page2';
+import { Page3 } from './components/page3/page3';
 export const routes: Routes = [
     {path:'' , component:Page1}
 ];
