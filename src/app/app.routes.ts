20ff419bae49a648a94b28d0d07cb9fc7e95926f
aishwarya <aishwaryaj1608@gmail.com>
2025-06-28 14:40:27 +0530

    Task completion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,5 @@ import { Page1 } from './components/page1/page1';
 import { Page2 } from './components/page2/page2';
 import { Page3 } from './components/page3/page3';
 export const routes: Routes = [
-    {path:'' , component:Page3}
+    {path:'' , component:Dashboard}
 ];
--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { Page1 } from '../page1/page1';
+import { Page2 } from '../page2/page2';
+import { Page3 } from '../page3/page3';
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [Page1 , Page2, Page3],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
