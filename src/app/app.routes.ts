50c2ba627487c3e70847bbedccb069b2ec1b041b
aishwarya <aishwaryaj1608@gmail.com>
2025-06-28 12:36:38 +0530

    spedhometask

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Page1 } from './components/page1/page1';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'' , component:Page1}
+];
